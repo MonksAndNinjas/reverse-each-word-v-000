67295f93682d8ec47eba06fe7d6dadecb88b02e1
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 01:52:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 
-def reverse_each_word(sentence)
-  words = sentence.scan(/\w\w?*\S\w?/)
-  word_reverse = []
-  words.each do |word|
-    word_reverse << word.reverse
-  end
-  word_reverse.join(" ")
-end
+#def reverse_each_word(sentence)
+#  words = sentence.scan(/\w\w?*\S\w?/)
+#  word_reverse = []
+#  words.each do |word|
+#    word_reverse << word.reverse
+#  end
+#  word_reverse.join(" ")
+#end
 
 def reverse_each_word(sentence)
   words = sentence.scan(/\w\w?*\S\w?/)
